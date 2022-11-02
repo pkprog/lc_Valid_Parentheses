bb271dd895f3822e919b361bf5675e979e0de0ee
pkprog <pk.prog@yandex.ru>
2022-11-03 00:21:12 +0300

    начало

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,38 @@
 public class Solution {
+    
+    enum Bracket {
+        T(""),
+        C,
+        S;
+        
+        private char value;
+        
+        private Bracket(char c) {
+            this.value = c;
+        }
+    }
 
-    int counter =
+    int counter = 0;
 
     public boolean isValid(String s) {
-
+        if (s.length() % 2 != 0) return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return true;
     }
 
 }
